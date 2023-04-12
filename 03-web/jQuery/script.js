f37ecd0f88f513336635d4c3731f6c8e068d32e3
scid05 <102736020+scid05@users.noreply.github.com>
2023-04-12 10:47:28 +0900

    Add files via upload

--- 03-web/jQuery/script.js
+++ 03-web/jQuery/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  $(".menu").hover(
+    function () {
+      $(".sub li").stop().slideDown();
+    },
+    function () {
+      $(".sub li").stop().slideUp();
+    }
+  );
+
   var i = 3;
   setInterval(function () {
     if (i == 3) $(".slide img").css({ display: "block" });
